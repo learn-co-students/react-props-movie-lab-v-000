9808895edc8603dcb14ef669f93b9c29e81ce63e
Dalynn4 <keith_adams007@yahoo.com>
2018-08-04 17:03:23 -0400

    Done.

--- test/3-CardBack.test.js
+++ test/3-CardBack.test.js
@@ -20,21 +20,5 @@ describe('<CardBack />', () => {
   const cardBack = shallow(<CardBack title="Rolling in the Deep" genres={genres} IMDBRating={3} />)
   const text = cardBack.text()
   
-  it("correctly renders the title prop", () => {
-    expect(text).to.contain('Rolling in the Deep')
-  })
-  
-  it("correctly renders the genres prop as comma seperated strings", () => {
-    expect(text).to.contain(genres.join(', '))
-  })
-  
-  it("renders 'No Rating Found' in an <h4> if the IMDBRating prop is null", () => {
-    const nullRating = shallow(<CardBack title="Rolling in the Deep" genres={['s']} IMDBRating={null} />)
-    expect(nullRating.find('h4').text().toLowerCase()).to.equal('no rating found')
-  })
-  
-  it("renders an <img> tag if the IMDBRating prop is valid", () => {
-    expect(cardBack.find('img').exists()).to.equal(true)
-  })
-  
+
 })
