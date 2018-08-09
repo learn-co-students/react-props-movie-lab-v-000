c80d0dee068771159af9c3ce15e8c04942cc81f0
github username <github email address>
2018-08-09 15:34:49 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import movieData from './data.js' // because you're iterating over it below
 
 export default class MovieShowcase extends Component {
 
-  generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+  generateMovieCards = () => { return movieData.map((data) => <MovieCard {...data} />) } // so you gotta import
+  // return a movie card for each movie, generate from movie data. not sure what {...date does though}
 
   render() {
     return (
@@ -16,3 +15,6 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+// .map over the imported movie data array and render MovieCards for each element. (see documentaion)
+// Don't forget to pass all 4 props
