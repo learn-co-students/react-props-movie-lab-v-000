3620b4c18cc7322927b120ddaaa37da2e57616e2
Bechev <bertrand.chevalier@gmail.com>
2018-09-01 13:07:45 -0400

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -32,11 +32,8 @@ export default class MovieCard extends Component {
   render() {
     return (
       <div className="movie-card">
-        <img src={this.props.posterSrc} alt={this.props.title} />
-        <h2>{this.props.title}</h2>
-        <small>{this.props.genres.join(', ')}</small>
-        <CardFront />
-        <CardBack />
+        <CardFront poster={posterMap[this.props.poster]}/>
+        <CardBack title={this.props.title} IMDBRating={this.props.IMDBRating} genres = {this.props.genres}/>
       </div>
     )
   }
