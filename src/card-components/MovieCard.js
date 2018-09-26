27ac060ae4220c03caafb09dd82997adffc91fbe
Kazumi Karbowski <kazkazinus@gmail.com>
2018-09-26 14:06:05 -0400

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -41,3 +41,10 @@ export default class MovieCard extends Component {
 }
 
 // Don't forget your default props!
+MovieCard.defaultProps = {
+  title: 'unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default'
+}
+
