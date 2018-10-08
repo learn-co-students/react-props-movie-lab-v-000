ca4249ceed06c3467ea6c7a7bdbe39d611ac6c1f
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-10-08 10:57:23 -0700

    All tests passing

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,11 +6,9 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return the correct 
-  //  let movieCards = movieData.map ( function (movie) {
-      return <MovieCard  title={movieData[0].title} IMDBRating={movieData[0].IMDBRating} genres={movieData[0].genres} poster={movieData[0].poster} />
-  //  });
-  //  console.log("gen cards", movieCards.join(' '));
-  //  return movieCards.join(' ');
+    return movieData.map ( (movie) => 
+      <MovieCard  title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster} />
+    );
   }
 
   render() {
