64e3ccd3e5860180fb14b0625d761e93988fdb3b
julianjohannesen <github email address>
2018-10-16 11:28:11 -0400

    Done.

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export default class CardFront extends Component {
 
   render() {
+    // Random text b/c learn submit won't submit
     return (
       <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
       </div>
