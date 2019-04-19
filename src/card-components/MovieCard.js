9d55736a68749b2a2805afbd4db8807db8832c04
Dani Lyn <dlyn1110@gmail.com>
2019-04-19 02:18:01 +0000

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -40,4 +40,11 @@ export default class MovieCard extends Component {
   }
 }
 
+MovieCard.defaultProps = {
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default'
+}
+
 // Don't forget your default props!
