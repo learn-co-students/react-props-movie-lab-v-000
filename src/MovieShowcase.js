39a36ce6b071574d1d0798f6a81005e21782d5c6
Garett Arrowood <garettarrowood@gmail.com>
2019-07-19 08:19:28 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,9 +3,13 @@ import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    return movieData.map((movie) => {
+      return <MovieCard title={movie.title}
+                   IMDBRating={movie.IMDBRating}
+                   genres={movie.genres}
+                   poster={movie.poster} />
+    })
   }
 
   render() {
--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -4,7 +4,11 @@ export default class CardFront extends Component {
 
   render() {
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={
+         {
+           backgroundImage: `url(${this.props.poster})`
+         }
+       }>
       </div>
     )
   }
