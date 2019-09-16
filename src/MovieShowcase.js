3318f3489ad7615cff492a0d730549a47669d8f9
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-16 16:15:07 -0700

    Update MovieShowcase.js

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -20,3 +20,29 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+//or:
+// import React, { Component } from "react";
+// import MovieCard from "./card-components/MovieCard.js";
+// import movieData from "./data.js";
+
+// export default class MovieShowcase extends Component {
+//   generateMovieCards = () => {
+//     return movieData.map((movieProps, index) => {
+//       return (
+//         <MovieCard
+//           key={index}
+//           title={movieProps.title}
+//           IMDBRating={movieProps.IMDBRating}
+//           genres={movieProps.genres}
+//           poster={movieProps.poster}
+//         />
+//       );
+//     });
+//   };
+//   // map over your movieData array and return the correct
+
+//   render() {
+//     return <div id="movie-showcase">{this.generateMovieCards()}</div>;
+//   }
+// }
