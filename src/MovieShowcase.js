c9907ba39dc8ebcef458f60e0bbdee1584a54754
Lorenzo Covarrubias <lorenzocovarrubias@Lorenzos-MacBook-Pro.local>
2019-10-03 11:33:04 -0500

    Completed React components with props passing successfully from parent to child

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -4,13 +4,21 @@ import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
-  generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+  generateMovieCards = () =>
+    movieData.map((movie) => {
+      return <MovieCard
+      title={movie.title}
+      poster= {movie.poster}
+      genres={movie.genres}
+      IMDBRating={movie.IMDBRating} />
+    } )
+
+
 
   render() {
     return (
       <div id="movie-showcase">
+      <h1> This should generate the movie cards </h1>
         {this.generateMovieCards()}
       </div>
     )
