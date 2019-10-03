668af3a5f54838bb1f79e30048ca1779a4431539
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-10-03 13:41:24 +0000

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,13 +3,11 @@ import MovieCard from "./card-components/MovieCard.js";
 import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-  generateMovieCards = () => {
-    // map over your movieData array and return the correct
-    return movieData.map((movie, ind) => {
-      <MovieCard key={ind} {...movie} />;
-    });
-  };
-  render() {
-    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
-  }
+	generateMovieCards = () => {
+		return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+	};
+
+	render() {
+		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
+	}
 }
