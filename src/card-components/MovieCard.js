56e6c863f32997edfd77022cfccea6220853314e
github username <github email address>
2019-12-03 19:40:49 -0500

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -34,7 +34,7 @@ export default class MovieCard extends Component {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
+        <CardFront poster = {posterMap[this.props.poster]}  />
         <CardBack
           title={this.props.title}
           IMDBRating={this.props.IMDBRating}
