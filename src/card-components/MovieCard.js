81e002b0917ffadcec6129e8d1829627322ae5d2
emanao <emanao@me.com>
2021-04-28 21:19:18 -0400

    Pass MovieShowcase tests.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -33,11 +33,14 @@ export default class MovieCard extends Component {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster={this.props.poster} />
+        <CardBack  title={this.props.title} genres={this.props.genres} IMDBRating={this.props.IMDBRating} />
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+// MovieCard.defaultProps={
+//     poster={defaultPoster}
+// }
