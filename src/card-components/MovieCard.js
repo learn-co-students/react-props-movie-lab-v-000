0c2267cfbbb6f50835c205fec1f2fb31ff6cefc6
emanao <emanao@me.com>
2021-04-28 21:26:15 -0400

    Pass <MovieCard /> tests.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -41,6 +41,9 @@ export default class MovieCard extends Component {
 }
 
 // Don't forget your default props!
-// MovieCard.defaultProps={
-//     poster={defaultPoster}
-// }
+MovieCard.defaultProps={
+    title:"Unknown",
+    IMDBRating:null,
+    genres:["No Genre(s) Found"],
+    poster:"default"
+}
